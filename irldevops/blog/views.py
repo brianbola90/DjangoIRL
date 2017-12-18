fe9f08450071836cfb99cb6d1e4e477937206cfd
Brian <brian.mcnabola@gmil.com>
2017-12-18 07:20:48 +0000

    Fixed issue to allow only object owners to delete their posts.

--- irldevops/blog/views.py
+++ irldevops/blog/views.py
@@ -2,6 +2,7 @@ from django.urls import reverse_lazy
 from django.urls import reverse
 from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from .models import Post
 # Create your views here.
 
@@ -28,6 +29,13 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
         return reverse('blog:detail',
                        kwargs={'pk': self.object.pk})
 
+    def get_object(self, queryset=None):
+        """ Hook to ensure object is owned by request.user. """
+        obj = super(PostUpdateView, self).get_object()
+        if not obj.author == self.request.user:
+            raise PermissionDenied
+        return obj
+
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -45,3 +53,10 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = reverse_lazy('blog:list')
+
+    def get_object(self, queryset=None):
+        """ Hook to ensure object is owned by request.user. """
+        obj = super(PostDeleteView, self).get_object()
+        if not obj.author == self.request.user:
+            raise PermissionDenied
+        return obj
