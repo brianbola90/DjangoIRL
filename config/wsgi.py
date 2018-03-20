74f3e5d0df6ad2eae3e38fc0a803fd1d691bf013
Brian McNabola <brian.mcnabola@workday.com>
2018-03-20 17:08:18 +0000

    update wsgi
    Change-Id: Iad10862b6ee03a928b708f7843f7deabe2916598

--- config/wsgi.py
+++ config/wsgi.py
@@ -22,8 +22,8 @@ from django.core.wsgi import get_wsgi_application
 app_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
 sys.path.append(os.path.join(app_path, 'irldevops'))
 
-#if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
-#   from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+   from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
@@ -35,8 +35,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
-#if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
- #   application = Sentry(application)
+if os.environ.get('DJANGO_SETTINGS_MODULE') == 'config.settings.production':
+    application = Sentry(application)
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
