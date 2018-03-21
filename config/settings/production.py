04dd7d10ce5e8f5a201b3c1be6ce398a071c609b
Brian McNabola <brian.mcnabola@workday.com>
2018-03-21 10:07:42 +0000

    update wsgi
    Change-Id: I5aaf7248120c76a87973d8a06a768bbda9103c37

--- config/settings/production.py
+++ config/settings/production.py
@@ -85,7 +85,7 @@ AWS_EXPIRY = 60 * 60 * 24 * 7
 # either django-storage-redux or boto
 control = 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY, AWS_EXPIRY)
 AWS_HEADERS = {
-    'Cache-Control': bytes(control).encode('latin-1')
+    'Cache-Control': bytes(control, encoding='latin-1')
 }
 
 # URL that handles the media served from MEDIA_ROOT, used for managing
