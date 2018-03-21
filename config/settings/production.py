b8176e1f3da8509e0b329deae0087d4178c449a3
Brian McNabola <brian.mcnabola@workday.com>
2018-03-21 16:52:45 +0000

    update wsgi
    Change-Id: Ic6e003aec5181cf701663a45386ef40a9647b4b0

--- config/settings/production.py
+++ config/settings/production.py
@@ -83,10 +83,13 @@ AWS_EXPIRY = 60 * 60 * 24 * 7
 # Revert the following and use str after the above-mentioned bug is fixed in
 # either django-storage-redux or boto
 control = 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIRY, AWS_EXPIRY)
-AWS_HEADERS = {
-    'Cache-Control': bytes(control, encoding='latin-1')
-}
+# AWS_HEADERS = {
+#     'Cache-Control': bytes(control, encoding='latin-1')
+# }
 
+AWS_HEADERS = {
+    'Cache-Control': bytes(control)
+}
 # URL that handles the media served from MEDIA_ROOT, used for managing
 # stored files.
 
