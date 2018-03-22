ce4ae9c0dcb9f15ec6949bc365ecfc9d5a8a04e6
Brian McNabola <brian.mcnabola@workday.com>
2018-03-22 16:56:24 +0000

    updated to maal settings
    Change-Id: I30bb5319b2760efe0b727779f4c03743495d00c1

--- config/settings/production.py
+++ config/settings/production.py
@@ -119,7 +119,7 @@ COMPRESS_ENABLED = env.bool('COMPRESS_ENABLED', default=True)
 # EMAIL
 # ------------------------------------------------------------------------------
 DEFAULT_FROM_EMAIL = env('DJANGO_DEFAULT_FROM_EMAIL',
-                         default='Django_IRL <noreply@example.com>')
+                         default='Django_IRL <noreply@djangoirl.com>')
 EMAIL_SUBJECT_PREFIX = env('DJANGO_EMAIL_SUBJECT_PREFIX', default='[Django_IRL]')
 SERVER_EMAIL = env('DJANGO_SERVER_EMAIL', default=DEFAULT_FROM_EMAIL)
 
