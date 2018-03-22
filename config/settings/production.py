1c9c60c0c6b5c14fcb7db0c488555f84f303dd09
Brian McNabola <brian.mcnabola@workday.com>
2018-03-22 11:50:58 +0000

    updated requirements
    Change-Id: Ibc00e2c84302acbe3da3620fd9e2519a18668fb6

--- config/settings/production.py
+++ config/settings/production.py
@@ -99,6 +99,9 @@ DEFAULT_FILE_STORAGE = 'config.settings.production.MediaRootS3BotoStorage'
 from datetime import datetime
 MARKDOWNX_MEDIA_PATH = datetime.now().strftime('markdownx/%Y/%m/%d')
 MARKDOWNX_UPLOAD_CONTENT_TYPES = ['image/jpeg', 'image/png', 'image/svg+xml', 'image/gif',]
+MARKDOWNX_MARKDOWN_EXTENSIONS = [
+    'markdown.extensions.extra'
+]
 # Static Assets
 # ------------------------
 STATIC_URL = 'https://s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
