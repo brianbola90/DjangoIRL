d49abe41b0a8ffc75c8e8198b99b640fe284ff6d
Brian McNabola <brian.mcnabola@workday.com>
2018-03-25 14:48:30 +0100

    Google analytics
    Change-Id: I1abe18037c48e930a3f12e38d6ac42b002be97f9

--- irldevops/dashboard.py
+++ irldevops/dashboard.py
@@ -7,7 +7,7 @@ from jet.dashboard.dashboard_modules import google_analytics
 
 
 class CustomIndexDashboard(Dashboard):
-    columns = 3
+    columns = 2
 
     def init_with_context(self, context):
         self.available_children.append(modules.LinkList)
@@ -56,39 +56,6 @@ class CustomIndexDashboard(Dashboard):
             _('Recent Actions'),
             10,
             column=0,
-            order=1
-        ))
-
-        # append a feed module
-        self.children.append(modules.Feed(
-            _('Latest Django News'),
-            feed_url='http://www.djangoproject.com/rss/weblog/',
-            limit=5,
-            column=1,
-            order=1
-        ))
-
-        # append another link list module for "support".
-        self.children.append(modules.LinkList(
-            _('Support'),
-            children=[
-                {
-                    'title': _('Django documentation'),
-                    'url': 'http://docs.djangoproject.com/',
-                    'external': True,
-                },
-                {
-                    'title': _('Django "django-users" mailing list'),
-                    'url': 'http://groups.google.com/group/django-users',
-                    'external': True,
-                },
-                {
-                    'title': _('Django irc channel'),
-                    'url': 'irc://irc.freenode.net/django',
-                    'external': True,
-                },
-            ],
-            column=2,
             order=1
         ))
 
